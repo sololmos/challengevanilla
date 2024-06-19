ce21ac9c2c149fa22a330adad5bc1fbe74b4380c
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-06-19 13:11:33 -0300

    card details

--- src/components/seccion1/Section1.jsx
+++ src/components/seccion1/Section1.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./Section1.css";
 import CardSection from "./cardSection/CardSection";
+import cardsData from "./data";
 
 
 
 const Section1 =()=>{
     return(
         <div className="section1-cont">
-            <div>
-                <CardSection></CardSection>
+            <div className="cards">
+            {cardsData.map((card) => (
+                <CardSection 
+                    key={card.id} 
+                    title={card.title} 
+                    icon={card.icon} 
+                    text={card.text} 
+                />
+            ))}
+
             </div>
+            
+                
+            
 
         </div>
     )
---
+++ src/components/seccion1/data.js
@@ -0,0 +1,24 @@
+
+import icon from "../../assets/Cohete.svg"
+const cardsData = [
+    {
+        id: 1,
+        title: "Item 1",
+        icon: icon, 
+        text: "Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje). Mientras puedes navegar en nuestro sitio antiguo."
+    },
+    {
+        id: 2,
+        title: "Item 2",
+        icon: icon, 
+        text: "Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje). Mientras puedes navegar en nuestro sitio antiguo."
+    },
+    {
+        id: 3,
+        title: "Item 3",
+        icon: icon, 
+        text: "Aún nuestro sitio está en cosntrucción (somos muy perfeccionistas jeje). Mientras puedes navegar en nuestro sitio antiguo."
+    }
+];
+
+export default cardsData;
