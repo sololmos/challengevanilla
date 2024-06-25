410ed069e6dbad08628551db670cb9f08e4b8f32
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-06-25 02:11:39 -0300

    fixing deploy and elipses position

--- src/components/seccion1/Section1.jsx
+++ src/components/seccion1/Section1.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import "./Section1.css";
 import CardSection from "./cardSection/CardSection";
 import cardsData from "./data";
+import mediaelipse from "../../assets/Ellipse1.svg"
+import cuartodeelipse from "../../assets/Ellipse2.svg"
 
 const Section1 = () => {
   return (
     <div className="section1-cont">
+        <div className="elipses">
+        <img src={mediaelipse} alt="Media Elipse" className="media-elipse" />
+        <img src={cuartodeelipse} alt="Cuarto de Elipse" className="cuarto-elipse" />
+
+        </div>
+
       <div className="cards">
         {cardsData.map((card) => (
           <CardSection
